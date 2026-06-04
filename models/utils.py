2496764b7537da69fcf2a7b65648769fdefc337f
qin850 <2138858975@qq.com>
2026-06-04 18:39:07 +0800

    Add files via upload

---
+++ models/utils.py
@@ -0,0 +1,12 @@
+
+def get_attributes_info(name):
+    if "ip102" in name:
+        info = {
+            "input_dim": 85,
+            "n": 102,
+            "m": 12,
+            "g": 12
+        }
+    else:
+        info = {}
+    return info
